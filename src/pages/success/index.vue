7b7d931e7a6a1d69816daaf9e77b1344e0189b33
linxiaowang <1104220717@qq.com>
2025-06-26 20:39:17 +0800

    fix: url import

--- src/pages/success/index.vue
+++ src/pages/success/index.vue
@@ -2,11 +2,12 @@
 defineOptions({
   name: 'SuccessPage',
 })
+const pictrueUrl = ref<string>('/kfc-vwo50/kfc6.jpeg')
 </script>
 
 <template>
   <div flex flex-col items-center>
-    <img src="/kfc-vwo50/kfc6.jpeg" alt="" h-128 w-128>
+    <img :src="pictrueUrl" alt="" h-128 w-128>
     有你真好～
   </div>
 </template>
